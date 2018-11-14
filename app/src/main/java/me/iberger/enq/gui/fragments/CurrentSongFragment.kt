0773ee75fd024f44e55ecee5fb112871e2bc5438
Ivo <ivo-berger@outlook.com>
2018-11-14 22:53:42 +0100

    adds jmusicbot library

--- app/src/main/java/me/iberger/enq/gui/fragments/CurrentSongFragment.kt
+++ app/src/main/java/me/iberger/enq/gui/fragments/CurrentSongFragment.kt
@@ -5,13 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_current_song.*
+import kotlinx.android.synthetic.main.fragment_song.*
 import me.iberger.enq.R
 
 class CurrentSongFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_current_song, container, false)
+        return inflater.inflate(R.layout.fragment_song, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/me/iberger/enq/gui/fragments/QueueFragment.kt
+++ app/src/main/java/me/iberger/enq/gui/fragments/QueueFragment.kt
@@ -14,6 +14,6 @@ class QueueFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_queue, container, false)
+        return inflater.inflate(R.layout.fragment_song_list, container, false)
     }
 }
