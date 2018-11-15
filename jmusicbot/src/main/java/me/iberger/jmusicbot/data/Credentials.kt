1edee50ad4fc82c0683ec6b81fb2be71d64ec07e
Ivo <ivo-berger@outlook.com>
2018-11-16 00:07:02 +0100

    auth token (de)serialization functions

--- jmusicbot/src/main/java/me/iberger/jmusicbot/data/Credentials.kt
+++ jmusicbot/src/main/java/me/iberger/jmusicbot/data/Credentials.kt
@@ -1,12 +1,26 @@
 package me.iberger.jmusicbot.data
 
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+import timber.log.Timber
 import java.util.*
 
 sealed class Credentials {
     class Register(
         val name: String,
-        val userId: String = UUID.randomUUID().toString()
+        val uuid: String = UUID.randomUUID().toString()
     ) : Credentials()
 
-    class Token(val token: String) : Credentials()
+    class Token(val token: String) : Credentials() {
+        companion object {
+            @ToJson
+            fun toJson(token: Credentials.Token) = token.token
+
+            @FromJson
+            fun fromJson(token: String): Credentials.Token {
+                Timber.d("Creating Token: $token")
+                return Credentials.Token(token)
+            }
+        }
+    }
 }
