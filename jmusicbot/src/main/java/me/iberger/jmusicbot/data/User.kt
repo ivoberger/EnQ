4fbbce7e15e17fc0083b8082301ba65c4f9848a1
Ivo <ivo-berger@outlook.com>
2018-11-16 00:07:50 +0100

    adds user data class

--- jmusicbot/src/main/java/me/iberger/jmusicbot/data/User.kt
+++ jmusicbot/src/main/java/me/iberger/jmusicbot/data/User.kt
@@ -1,3 +1,34 @@
 package me.iberger.jmusicbot.data
 
-data class User(val name: String, var authToken: Credentials.Token)
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import com.squareup.moshi.JsonEncodingException
+import com.squareup.moshi.Moshi
+import me.iberger.jmusicbot.KEY_USER
+import timber.log.Timber
+
+
+data class User(val name: String, var authorization: Credentials.Token) {
+
+    init {
+        Timber.d("Creating User $this")
+    }
+
+    companion object {
+        fun load(sharedPreferences: SharedPreferences, moshi: Moshi = Moshi.Builder().build()): User? {
+            val userAdapter = moshi.adapter<User>(this::class.java)
+            sharedPreferences.getString(KEY_USER, null)?.let {
+                Timber.d("Creating user from json: $it")
+                return userAdapter.fromJson(it) ?: throw JsonEncodingException("User data corrupted")
+            }
+            return null
+        }
+    }
+
+    fun save(sharedPreferences: SharedPreferences) {
+        val userAdapter = Moshi.Builder().build().adapter<User>(this::class.java)
+        sharedPreferences.edit {
+            putString(KEY_USER, userAdapter.toJson(this@User))
+        }
+    }
+}
