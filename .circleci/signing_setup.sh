df618124264836241fe1e704f343550da371721c
Ivo <ivo-berger@outlook.com>
2018-11-24 14:09:32 +0100

    fix

---
+++ .circleci/signing_setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cat <<EOF > keystore.properties
+    storePassword=${SIGNING_KEYSTORE_PW}
+    keyPassword=${SIGNING_KEY_PW}
+    keyAlias=enq
+    storeFile=keystore.jks
+EOF
+echo ${SIGNING_KEYSTORE} | base64 -d keystore.jks
