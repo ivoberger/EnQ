edb8deac8b150d376eaebac94d85709451bfb692
Ivo Berger <ivo-berger@outlook.com>
2019-01-17 15:59:50 +0100

    upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
 }
 
 plugins {
-    id("jmfayard.github.io.gradle-kotlin-dsl-libs") version Versions.jmfayard_github_io_gradle_kotlin_dsl_libs_gradle_plugin
+    id("de.fayard.buildSrcVersions") version Versions.de_fayard_buildsrcversions_gradle_plugin
 }
 
 allprojects {
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,34 +2,32 @@ import kotlin.String
 
 /**
  * Find which updates are available by running
- *     `$ ./gradlew syncLibs`
+ *     `$ ./gradlew buildSrcVersions`
  * This will only update the comments.
  *
  * YOU are responsible for updating manually the dependency version. */
 object Versions {
-    const val appcompat: String = "1.1.0-alpha01" // exceed the version found: 1.0.2
+    const val appcompat: String = "1.1.0-alpha01" 
 
-    const val constraintlayout: String = "2.0.0-alpha2" // exceed the version found: 1.1.3
+    const val constraintlayout: String = "2.0.0-alpha2" 
 
     const val core_ktx: String = "1.0.1" 
 
-    const val fragment_ktx: String = "1.1.0-alpha02" // exceed the version found: 1.0.0
+    const val fragment_ktx: String = "1.1.0-alpha02" 
 
-    const val com_android_tools_build_gradle: String =
-            "3.4.0-alpha07" // exceed the version found: 3.2.1
+    const val com_android_tools_build_gradle: String = "3.5.0-alpha01" 
 
-    const val lint_gradle: String = "26.4.0-alpha07" // exceed the version found: 26.2.1
+    const val lint_gradle: String = "26.5.0-alpha01" 
 
-    const val jmusicbotandroid: String = "5aba4fe454" // exceed the version found: 0.2.0
+    const val jmusicbotandroid: String = "5aba4fe454" 
 
     const val timbersentry: String = "0.2.0" 
 
-    const val com_github_triplet_play_gradle_plugin: String = "2.0.0" 
+    const val com_github_triplet_play_gradle_plugin: String = "2.1.0" 
 
-    const val material: String = "1.1.0-alpha01" // exceed the version found: 1.0.0
+    const val material: String = "1.1.0-alpha01" 
 
-    const val community_material_typeface: String =
-            "3.1.0-rc02" // exceed the version found: 2.7.94.1
+    const val community_material_typeface: String = "3.1.0-rc02" 
 
     const val fastadapter_commons: String = "3.3.1" 
 
@@ -43,13 +41,13 @@ object Versions {
 
     const val picasso: String = "2.71828" 
 
-    const val sentry_android_gradle_plugin: String = "1.7.15" 
+    const val de_fayard_buildsrcversions_gradle_plugin: String = "0.3.2" 
 
-    const val jmfayard_github_io_gradle_kotlin_dsl_libs_gradle_plugin: String = "0.2.6" 
+    const val sentry_android_gradle_plugin: String = "1.7.16" 
 
-    const val org_jetbrains_kotlin: String = "1.3.11" // exceed the version found: 1.3.10
+    const val org_jetbrains_kotlin: String = "1.3.11" 
 
-    const val kotlinx_coroutines_android: String = "1.0.1" 
+    const val kotlinx_coroutines_android: String = "1.1.0" 
 
     const val core_kt: String = "1.0.3" 
 
@@ -59,11 +57,11 @@ object Versions {
      *      ./gradle/wrapper/gradle-wrapper.properties
      */
     object Gradle {
-        const val runningVersion: String = "5.0"
+        const val runningVersion: String = "5.1.1"
 
-        const val currentVersion: String = "5.0"
+        const val currentVersion: String = "5.1.1"
 
-        const val nightlyVersion: String = "5.2-20181210000034+0000"
+        const val nightlyVersion: String = "5.2-20190117003518+0000"
 
         const val releaseCandidate: String = ""
     }
