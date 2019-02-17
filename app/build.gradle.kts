41979ce0acd742ccf8f4b1e8b62e81533ff821aa
Ivo Berger <ivo-berger@outlook.com>
2019-02-17 09:15:32 +0100

    updates changelog and version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdkVersion(21)
         targetSdkVersion(28)
         versionCode = 2
-        versionName = "0.4.0"
+        versionName = "0.4.1"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
