0350e941ad344dbbcb0f0f2fe23348259ca30f61
Ivo Berger <ivo-berger@outlook.com>
2019-05-03 17:56:35 +0200

    sets glides disk cache to 100mb

--- app/src/main/java/com/ivoberger/enq/persistence/GlideModule.kt
+++ app/src/main/java/com/ivoberger/enq/persistence/GlideModule.kt
@@ -1,7 +1,16 @@
 package com.ivoberger.enq.persistence
 
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
 import com.bumptech.glide.module.AppGlideModule
 
+
 @GlideModule
-class GlideModule : AppGlideModule()
+class GlideModule : AppGlideModule() {
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        val diskCacheSizeBytes = 1024 * 1024 * 100 // 100 MB
+        builder.setDiskCache(InternalCacheDiskCacheFactory(context, diskCacheSizeBytes.toLong()))
+    }
+}
