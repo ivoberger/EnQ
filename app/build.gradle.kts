ff1fc9463536f346d2a7e474e45631cc26ec645a
Ivo Berger <ivo-berger@outlook.com>
2019-05-08 12:21:07 +0200

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdkVersion(21)
         targetSdkVersion(28)
         versionCode = 2
-        versionName = "0.6.6"
+        versionName = "0.7.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
